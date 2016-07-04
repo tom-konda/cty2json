b960a6acbff4c434f13d38f2bc2d0cb15075a496
tom-konda <tom.konda.dev@gmail.com>
2016-07-04 23:32:02 +0900

    Add a JSON file format definition.

--- src/test/custom_typings/cty2json/cty2json.d.ts
+++ src/test/custom_typings/cty2json/cty2json.d.ts
@@ -7,3 +7,54 @@ interface Cty2JSONStatic{
 declare module 'cty2json' {
   export = Cty2JSON
 }
+
+interface Cty2JSONFileFormat {
+  fileSize : number
+  historyDatas : {
+    res : number[],
+    com : number[],
+    ind : number[],
+    cri : number[],
+    pol : number[],
+    val : number[],
+  },
+  miscDatas : {
+    RPopulation: number,
+    CPopulation: number,
+    IPopulation: number,
+    RValve: number,
+    CValve: number,
+    IValve: number,
+    cityTime: number,
+    crimeRamp: number,
+    polluteRamp: number,
+    landValueAve: number,
+    crimeAve: number,
+    pollutionAve: number,
+    gameLevel: number,
+    cityClass: number,
+    cityScore: number,
+    budget: number,
+    autoBulldoze: number,
+    autoBudget: number,
+    autoGoto: number,
+    soundOn: number,
+    tax: number,
+    gameSpeed: number,
+    policeCovered: number,
+    fireCovered: number,
+    roadCovered: number
+  },
+  tileDatas : [
+    [
+      {
+        building: number,
+        zoneCenter: number,
+        animated: number,
+        bulldozable: number,
+        combustible: number,
+        conductive: number,
+      }
+    ]
+  ]
+}

2e0dda0debfef3efcaab84df4566d154b3b886c8
tom-konda <tom.konda.dev@gmail.com>
2016-12-17 19:51:30 +0900

    Change require parameter.

--- src/ts/bin/cli.ts
+++ src/ts/bin/cli.ts
@@ -5,14 +5,14 @@
 * Released under the GPLv3 license
 * See https://www.gnu.org/licenses/gpl-3.0.en.html
 */
-require('../lib/cty2json.js');
+const Cty2JSON = <Cty2JSONStatic>require('../index');
 import commander = require('commander');
 import fs = require('fs');
 
-const fileAccessCheck = (inputFile:string) => {
+const fileAccessCheck = (inputFile: string) => {
   return new Promise(
     (resolve, reject) => {
-      let isfileReadable:number;
+      let isfileReadable: number;
       if (typeof fs.constants === 'undefined') {
         // for Node 4.x
         isfileReadable = fs.R_OK;
@@ -31,7 +31,7 @@ const fileAccessCheck = (inputFile:string) => {
   );
 }
 
-const fileFormatCheck = (inputfile:string) => {
+const fileFormatCheck = (inputfile: string) => {
   return new Promise(
     (resolve, reject) => {
       const file = fs.readFileSync(inputfile);
@@ -48,7 +48,7 @@ const fileFormatCheck = (inputfile:string) => {
   );
 }
 
-const outputJSON = (jsonObj:any, options:any) => {
+const outputJSON = (jsonObj: any, options: any) => {
   return new Promise(
     (resolve, reject) => {
       const JSONtext = JSON.stringify(jsonObj, null, '  ');
@@ -70,32 +70,32 @@ const outputJSON = (jsonObj:any, options:any) => {
   )
 }
 
-const convertCty2JSON = function (inputCTYFile:string, options:any) {
+const convertCty2JSON = function (inputCTYFile: string, options: any) {
   fileAccessCheck(inputCTYFile)
     .then(
-      fileFormatCheck,
-      (error) => {
-        console.error('Cannot Read File.');
-        process.stderr.write(`${error.errno} : ${error.message}\n`);
-        process.exit(error.errno);
-      }
+    fileFormatCheck,
+    (error) => {
+      console.error('Cannot Read File.');
+      process.stderr.write(`${error.errno} : ${error.message}\n`);
+      process.exit(error.errno);
+    }
     ).then(
-      (json) => outputJSON(json, options),
-      (error:SyntaxError) => {
-        console.error('This is wrong Micropolis cty file.');
-        console.error(error);
-        process.stderr.write(`${error.name} : ${error.message}\n`);
-        process.exit(1);
-      }
+    (json) => outputJSON(json, options),
+    (error: SyntaxError) => {
+      console.error('This is wrong Micropolis cty file.');
+      console.error(error);
+      process.stderr.write(`${error.name} : ${error.message}\n`);
+      process.exit(1);
+    }
     ).then(
-      (outputFile) => {
-        outputFile ? process.stdout.write(`${outputFile} was created successfully.`) : null;
-      },
-      (error) => {
-        console.error('Cannot write a JSON file.');
-        process.stderr.write(`${error.errno} : ${error.message}\n`);
-        process.exit(error.errno);
-      }
+    (outputFile) => {
+      outputFile ? process.stdout.write(`${outputFile} was created successfully.`) : null;
+    },
+    (error) => {
+      console.error('Cannot write a JSON file.');
+      process.stderr.write(`${error.errno} : ${error.message}\n`);
+      process.exit(error.errno);
+    }
     );
 }
 

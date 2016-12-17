f40b3a938646170c98d12254da6d976596ea97d7
tom-konda <tom.konda.dev@gmail.com>
2016-12-17 19:14:17 +0900

    Format file.

--- src/ts/lib/custom_typings/cty2json/cty2json.d.ts
+++ src/ts/lib/custom_typings/cty2json/cty2json.d.ts
@@ -1,25 +1,21 @@
 interface tileInterface {
-  building : number,
-  zoneCenter : number,
-  animated : number,
-  bulldozable : number,
-  combustible : number,
-  conductive : number,
+    building: number,
+    zoneCenter: number,
+    animated: number,
+    bulldozable: number,
+    combustible: number,
+    conductive: number,
 }
 
 interface cty2JSONDataFormat {
-      fileSize: number,
-      historyData: {[index: string]: number[]},
-      miscData: {[index : string]:number},
-      tileData: {}[][],
+    fileSize: number,
+    historyData: { [index: string]: number[] },
+    miscData: { [index: string]: number },
+    tileData: { [index: number]: tileInterface[] },
 }
 
-declare let Cty2JSON : Cty2JSONStatic;
-
-interface Cty2JSONStatic{
-  analyzeData (data:ArrayBuffer): string;
+interface Cty2JSONStatic {
+    analyzeData(data: ArrayBuffer): string;
 }
 
-declare module 'cty2json' {
-  export = Cty2JSON
-}
+declare const Cty2JSON: Cty2JSONStatic;

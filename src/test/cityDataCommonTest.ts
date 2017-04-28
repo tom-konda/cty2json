3f392a74da4c543ac318e708a4fabb709e63d030
tom-konda <tom.konda.dev@gmail.com>
2017-04-28 10:12:34 +0900

    Replace text from 'power-assert' to 'assert'

--- src/test/cityDataCommonTest.ts
+++ src/test/cityDataCommonTest.ts
@@ -1,4 +1,4 @@
-import assert = require('power-assert');
+import assert = require('assert');
 
 export const checkCityData = (cityData: any) => {
   assert.deepEqual(cityData.miscData.budget, 10560, 'File is not analyzed correctly.');

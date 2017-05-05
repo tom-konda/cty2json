3e6277d159d2a93ca773fa1e921e36affc40ec5a
tom-konda <tom.konda.dev@gmail.com>
2017-05-05 19:46:58 +0900

    Fix jsdom test.

--- src/test/jsdomTest.ts
+++ src/test/jsdomTest.ts
@@ -1,19 +1,20 @@
 'use strict';
 
 import jsdom = require('jsdom');
+const { JSDOM } = jsdom as any;
 import fs = require('fs');
 const file = fs.readFileSync(`${__dirname}/fixture/cty2jsonTest.cty`);
 const base64 = file.toString('base64');
 import cityDataCommonTest = require('./cityDataCommonTest');
 const Cty2JSON = <Cty2JSONStatic>require('../../lib/cty2json.cjs');
 
-const doc = jsdom.jsdom(
+const doc = new JSDOM(
   '<html><head></head><body></body></html>',
   {
   }
 );
 
-const win = doc.defaultView;
+const win = doc.window;
 const dataURLSch = `data:application/octet-binary;base64,${base64}`;
 
 const xhr = new win.XMLHttpRequest();

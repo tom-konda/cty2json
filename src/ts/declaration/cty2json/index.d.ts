d0df5698c26a8ced8e3f487be1fd3e7c56b8b1bd
tom-konda <tom.konda.dev@gmail.com>
2018-04-11 01:30:54 +0900

    Simplify declaration file

--- src/ts/declaration/cty2json/index.d.ts
+++ src/ts/declaration/cty2json/index.d.ts
@@ -1,4 +1,16 @@
-interface tileInterface {
+type cty2JSONDataFormat = {
+  fileSize: number,
+  historyData: historyData,
+  miscData: miscData,
+  tileData: tileData[][],
+}
+
+type Cty2JSONStatic = {
+  analyze(data: ArrayBuffer): cty2JSONDataFormat;
+  outputJSONText(data: ArrayBuffer): string;
+}
+
+type tileData = {
   building: number,
   zoneCenter: boolean,
   animated: boolean,
@@ -7,77 +19,49 @@ interface tileInterface {
   conductive: boolean,
 }
 
-interface cty2JSONDataFormat {
-  fileSize: number,
-  historyData: {
-    [index: string]: historyGraphData,
-  },
-  miscData: { [index: string]: number },
-  tileData: tileInterface[][],
+type historyData = {
+  residential: historyGraphData,
+  commercial: historyGraphData,
+  industrial: historyGraphData,
+  crime: historyGraphData,
+  pollution: historyGraphData,
+  landValue: historyGraphData,
 }
 
-interface Cty2JSONStatic {
-  analyze(data: ArrayBuffer): cty2JSONDataFormat;
-  outputJSONText(data: ArrayBuffer): string;
+type historyGraphData = {
+  '10years': number[],
+  '120years': number[],
+}
+
+type miscData = {
+  RPopulation: number,
+  CPopulation: number,
+  IPopulation: number,
+  RValve: number,
+  CValve: number,
+  IValve: number,
+  cityTime: number,
+  crimeRamp: number,
+  polluteRamp: number,
+  landValueAve: number,
+  crimeAve: number,
+  pollutionAve: number,
+  gameLevel: number,
+  cityClass: number,
+  cityScore: number,
+  budget: number,
+  autoBulldoze: number,
+  autoBudget: number,
+  autoGoto: number,
+  soundOn: number,
+  tax: number,
+  gameSpeed: number,
+  policeCovered: number,
+  fireCovered: number,
+  transportCovered: number
 }
 
 declare const Cty2JSON: Cty2JSONStatic;
 declare module 'cty2json' {
   export = Cty2JSON;
-}
-
-interface historyGraphData {
-  '10years': number[],
-  '120years': number[],
-}
-
-interface Cty2JSONFileDetailFormat {
-  fileSize: number
-  historyData: {
-    residential: historyGraphData,
-    commercial: historyGraphData,
-    industrial: historyGraphData,
-    crime: historyGraphData,
-    pollution: historyGraphData,
-    landValue: historyGraphData,
-  },
-  miscData: {
-    RPopulation: number,
-    CPopulation: number,
-    IPopulation: number,
-    RValve: number,
-    CValve: number,
-    IValve: number,
-    cityTime: number,
-    crimeRamp: number,
-    polluteRamp: number,
-    landValueAve: number,
-    crimeAve: number,
-    pollutionAve: number,
-    gameLevel: number,
-    cityClass: number,
-    cityScore: number,
-    budget: number,
-    autoBulldoze: number,
-    autoBudget: number,
-    autoGoto: number,
-    soundOn: number,
-    tax: number,
-    gameSpeed: number,
-    policeCovered: number,
-    fireCovered: number,
-    transportCovered: number
-  },
-  tileData: [
-    [
-      {
-        building: number,
-        zoneCenter: boolean,
-        animated: boolean,
-        bulldozable: boolean,
-        combustible: boolean,
-        conductive: boolean,
-      }
-    ]
-  ]
 }

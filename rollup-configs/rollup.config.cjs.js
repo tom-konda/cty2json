fe0e5727ea98eae1de55b90f3cf2b615e5831e1d
Tom Konda <tom.konda.dev@gmail.com>
2022-06-25 02:41:48 +0900

    Remove unused files

--- rollup-configs/rollup.config.cjs.js
+++
@@ -1,9 +0,0 @@
-export default {
-  input: './lib/cty2json.js',
-  external: ['fs'],
-  output: [
-    { file: 'lib/cty2json.cjs.js', format: 'cjs' },
-    { file: 'index.js', format: 'cjs' },
-  ],
-  plugins: []
-}
